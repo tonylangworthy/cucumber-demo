307c20a6918c34561dd1690f5d5d03c01bd275a2
Tony Langworthy <tony.langworthy@zealitconsultants.com>
2023-05-02 15:06:33 -0500

    TWL - Team Commit WIP

--- src/test/java/org/example/StepDefinitions.java
+++ src/test/java/org/example/StepDefinitions.java
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
 }
