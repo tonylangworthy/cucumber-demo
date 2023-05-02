03976cf5ba740c223a50e9152c40bfe0090c9478
Tony Langworthy <tony.langworthy@zealitconsultants.com>
2023-05-02 16:15:52 -0500

    TWL - Updated the step definitions to pass.

--- src/test/java/org/example/StepDefinitions.java
+++ src/test/java/org/example/StepDefinitions.java
@@ -4,22 +4,31 @@ import io.cucumber.java.en.*;
 
 import org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+}
+
 public class StepDefinitions {
+
+    private String today;
+
+    private String actualAnswer;
 
     @Given("today is Sunday")
     public void today_is_sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        today = "Sunday";
     }
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actualAnswer = IsItFriday.isItFriday(today);
     }
     @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 
 }
